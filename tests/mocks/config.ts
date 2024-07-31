f35374e24dc383cf08cd97748adc39e5668ea9a9
nitzanmorr <nitzan.morr@gmail.com>
2024-07-31 16:05:04 +0300

    test: modified unit tests to use heartbeat check as an option and enhanced config mock

--- tests/mocks/config.ts
+++ tests/mocks/config.ts
@@ -1,11 +1,70 @@
+import config from 'config';
+import { get, has } from 'lodash';
 import { IConfig } from '../../src/common/interfaces';
 
+let mockConfig: Record<string, unknown> = {};
 const getMock = jest.fn();
 const hasMock = jest.fn();
 
 const configMock = {
   get: getMock,
   has: hasMock,
-} as unknown as IConfig;
+} as IConfig;
 
-export { getMock, hasMock, configMock };
+const init = (): void => {
+  getMock.mockImplementation((key: string): unknown => {
+    return mockConfig[key] ?? config.get(key);
+  });
+};
+
+const setValue = (key: string | Record<string, unknown>, value?: unknown): void => {
+  if (typeof key === 'string') {
+    mockConfig[key] = value;
+  } else {
+    mockConfig = { ...mockConfig, ...key };
+  }
+};
+
+const clear = (): void => {
+  mockConfig = {};
+};
+
+const setConfigValues = (values: Record<string, unknown>): void => {
+  getMock.mockImplementation((key: string) => {
+    const value = get(values, key) ?? config.get(key);
+    return value;
+  });
+  hasMock.mockImplementation((key: string) => has(values, key) || config.has(key));
+};
+
+const registerDefaultConfig = (): void => {
+  const config = {
+    logger: {
+      level: 'info',
+    },
+    jobServiceUrl: '',
+    heartbeat: {
+      enabled: true,
+      failedDurationMS: 1000,
+      serviceUrl: '',
+    },
+    updateTime: {
+      enabled: true,
+      failedDurationSec: 300,
+      taskTypes: [],
+      ignoredTaskTypes: [],
+      checkHeartbeat: false,
+    },
+    expirationStatus: {
+      enabled: true,
+    },
+    httpRetry: {
+      attempts: 3,
+      delay: 'exponential',
+      shouldResetTimeout: true,
+    },
+  };
+  setConfigValues(config);
+};
+
+export { getMock, hasMock, configMock, setValue, clear, init, setConfigValues, registerDefaultConfig };
--- tests/unit/updateTime/updateTimeReleaser.spec.ts
+++ tests/unit/updateTime/updateTimeReleaser.spec.ts
@@ -2,7 +2,7 @@ import { v4 as uuidv4 } from 'uuid';
 import jsLogger from '@map-colonies/js-logger';
 import { UpdateTimeReleaser } from '../../../src/updateTime/updateTimeReleaser';
 import { tracerMock, initTrace } from '../../mocks/openTelemetry/tracer';
-import { configMock, getMock } from '../../mocks/config';
+import { configMock, getMock, setValue, init as initconfig, clear as clearConfig } from '../../mocks/config';
 import { tasksClientMock, tasksReleaseTasksMock, tasksInactiveTasksMock } from '../../mocks/clients/tasksClient';
 import { heartbeatClientMock, getHeartbeatMock } from '../../mocks/clients/heartbeatClient';
 import { NotFoundError } from '../../../src/common/exceptions/http/notFoundError';
@@ -12,16 +12,19 @@ let releaser: UpdateTimeReleaser;
 describe('UpdateTimeReleaser', () => {
   beforeEach(function () {
     initTrace();
+    initconfig();
+    setValue('updateTime.enabled', true);
   });
 
   afterEach(function () {
+    clearConfig();
     jest.resetAllMocks();
   });
 
   describe('run', () => {
     it('do nothing when disabled', async function () {
       //mock data
-      getMock.mockReturnValue('false');
+      setValue('updateTime.enabled', false);
 
       // action
       releaser = new UpdateTimeReleaser(configMock, jsLogger({ enabled: false }), tracerMock, tasksClientMock, heartbeatClientMock);
@@ -29,14 +32,14 @@ describe('UpdateTimeReleaser', () => {
 
       // expectation
       expect(getMock).toHaveBeenCalledWith('updateTime.enabled');
-      expect(getMock).toHaveBeenCalledTimes(1);
+      expect(getMock).toHaveBeenCalledWith('updateTime.checkHeartbeat');
+      expect(getMock).toHaveBeenCalledTimes(2);
       expect(tasksInactiveTasksMock).not.toHaveBeenCalled();
       expect(tasksReleaseTasksMock).not.toHaveBeenCalled();
     });
 
     it('do nothing when there are no tasks stuck in in-progress', async function () {
       //mock data
-      getMock.mockReturnValueOnce(true);
       tasksInactiveTasksMock.mockResolvedValue([]);
       // action
       releaser = new UpdateTimeReleaser(configMock, jsLogger({ enabled: false }), tracerMock, tasksClientMock, heartbeatClientMock);
@@ -44,15 +47,18 @@ describe('UpdateTimeReleaser', () => {
 
       // expectation
       expect(getMock).toHaveBeenCalledWith('updateTime.enabled');
-      expect(getMock).toHaveBeenCalledTimes(1);
+      expect(getMock).toHaveBeenCalledWith('updateTime.checkHeartbeat');
+      expect(getMock).toHaveBeenCalledTimes(2);
       expect(tasksInactiveTasksMock).toHaveBeenCalledTimes(1);
       expect(tasksReleaseTasksMock).not.toHaveBeenCalled();
     });
 
-    it('do nothing when there are tasks stuck in in-progress but had a heartbeat', async function () {
+    it('release dead task when heartbeat check is disabled and task is inactive but had a heartbeat', async function () {
       //mock data
-      getMock.mockReturnValueOnce(true);
-      tasksInactiveTasksMock.mockResolvedValue([uuidv4()]);
+      const deadTasks = ['dead', 'completed'];
+      setValue('updateTime.checkHeartbeat', false);
+      tasksInactiveTasksMock.mockResolvedValue(deadTasks);
+      tasksReleaseTasksMock.mockResolvedValue([]);
       getHeartbeatMock.mockResolvedValue({});
       // action
       releaser = new UpdateTimeReleaser(configMock, jsLogger({ enabled: false }), tracerMock, tasksClientMock, heartbeatClientMock);
@@ -60,32 +66,56 @@ describe('UpdateTimeReleaser', () => {
 
       // expectation
       expect(getMock).toHaveBeenCalledWith('updateTime.enabled');
-      expect(getMock).toHaveBeenCalledTimes(1);
-      expect(tasksInactiveTasksMock).toHaveBeenCalledTimes(1);
-      expect(getHeartbeatMock).toHaveBeenCalledTimes(1);
-      expect(tasksReleaseTasksMock).not.toHaveBeenCalled();
-    });
-
-    it('release dead tasks', async function () {
-      //mock data
-      const deadTasks = ['dead', 'completed'];
-      getMock.mockReturnValue(true);
-      tasksInactiveTasksMock.mockResolvedValue(deadTasks);
-      tasksReleaseTasksMock.mockResolvedValue([]);
-      getHeartbeatMock.mockImplementation(() => {
-        throw new NotFoundError('No Heartbeats found');
-      });
-      // action
-      releaser = new UpdateTimeReleaser(configMock, jsLogger({ enabled: false }), tracerMock, tasksClientMock, heartbeatClientMock);
-      await releaser.run();
-
-      // expectation
-      expect(getMock).toHaveBeenCalledWith('updateTime.enabled');
-      expect(getMock).toHaveBeenCalledTimes(1);
+      expect(getMock).toHaveBeenCalledWith('updateTime.checkHeartbeat');
+      expect(getMock).toHaveBeenCalledTimes(2);
       expect(tasksInactiveTasksMock).toHaveBeenCalledTimes(1);
       expect(tasksReleaseTasksMock).toHaveBeenCalledTimes(1);
       expect(tasksReleaseTasksMock).toHaveBeenCalledWith(deadTasks);
-      expect(getHeartbeatMock).toThrow(NotFoundError);
+      expect(getHeartbeatMock).not.toHaveBeenCalled();
+    });
+
+    describe('heartbeat check enabled', () => {
+      beforeEach(function () {
+        setValue('updateTime.checkHeartbeat', true);
+      });
+      it('do nothing when there are tasks stuck in in-progress but had a heartbeat', async function () {
+        //mock data
+        tasksInactiveTasksMock.mockResolvedValue([uuidv4()]);
+        getHeartbeatMock.mockResolvedValue({});
+        // action
+        releaser = new UpdateTimeReleaser(configMock, jsLogger({ enabled: false }), tracerMock, tasksClientMock, heartbeatClientMock);
+        await releaser.run();
+
+        // expectation
+        expect(getMock).toHaveBeenCalledWith('updateTime.enabled');
+        expect(getMock).toHaveBeenCalledWith('updateTime.checkHeartbeat');
+        expect(getMock).toHaveBeenCalledTimes(2);
+        expect(tasksInactiveTasksMock).toHaveBeenCalledTimes(1);
+        expect(getHeartbeatMock).toHaveBeenCalledTimes(1);
+        expect(tasksReleaseTasksMock).not.toHaveBeenCalled();
+      });
+
+      it('release dead tasks that never had a heartbeat', async function () {
+        //mock data
+        const deadTasks = ['dead', 'completed'];
+        tasksInactiveTasksMock.mockResolvedValue(deadTasks);
+        tasksReleaseTasksMock.mockResolvedValue([]);
+        getHeartbeatMock.mockImplementation(() => {
+          throw new NotFoundError('No Heartbeats found');
+        });
+        // action
+        releaser = new UpdateTimeReleaser(configMock, jsLogger({ enabled: false }), tracerMock, tasksClientMock, heartbeatClientMock);
+        await releaser.run();
+
+        // expectation
+        expect(getMock).toHaveBeenCalledWith('updateTime.enabled');
+        expect(getMock).toHaveBeenCalledWith('updateTime.checkHeartbeat');
+        expect(getMock).toHaveBeenCalledTimes(2);
+        expect(tasksInactiveTasksMock).toHaveBeenCalledTimes(1);
+        expect(tasksReleaseTasksMock).toHaveBeenCalledTimes(1);
+        expect(tasksReleaseTasksMock).toHaveBeenCalledWith(deadTasks);
+        expect(getHeartbeatMock).toThrow(NotFoundError);
+      });
     });
   });
 });
