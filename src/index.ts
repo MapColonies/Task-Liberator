3dcaa0f4d4673c4559fb86c0f5979f8d67cdf73a
netanelC <netanel153@gmail.com>
2025-02-12 12:56:56 +0200

    refactor: use promised timers

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-/* eslint-disable import/first */
 // this import must be called before the first import of tsyring
 import 'reflect-metadata';
+import { setInterval } from 'node:timers/promises';
 import { Tracing } from '@map-colonies/telemetry';
 import { Logger } from '@map-colonies/js-logger';
 import config from 'config';
@@ -12,14 +12,8 @@ import { UpdateTimeReleaser } from './updateTime/updateTimeReleaser';
 import { HeartbeatReleaser } from './heartbeat/heartbeatReleaser';
 import { ExpirationStatusUpdater } from './taskExpiration/expirartionStatusUpdater';
 
-let isActive = false;
 
 async function run(logger: Logger): Promise<void> {
-  if (isActive) {
-    return;
-  }
-  isActive = true;
-
   try {
     await container.resolve(UpdateTimeReleaser).run();
   } catch (err) {
@@ -38,29 +32,29 @@ async function run(logger: Logger): Promise<void> {
     const error = err as Error;
     logger.error(error.message);
   }
-
-  isActive = false;
 }
 
-function main(): void {
+async function main(): Promise<void> {
   const tracing = new Tracing([
     new HttpInstrumentation({ ignoreOutgoingUrls: IGNORED_OUTGOING_TRACE_ROUTES, ignoreIncomingPaths: IGNORED_INCOMING_TRACE_ROUTES }),
   ]);
 
   registerExternalValues(tracing);
   const logger = container.resolve<Logger>(Services.LOGGER);
-  setInterval(() => {
-    void (async (): Promise<void> => {
-      try {
-        await run(logger);
-      } catch (err) {
-        const error = err as Error;
-        logger.error({ msg: `Main Loop error: ${error.message}`, err });
-      }
-    })();
-  }, config.get<number>('intervalMs'));
+  const interval = config.get<number>('intervalMs');
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/naming-convention
+  for await (const _ of setInterval(interval, null)) {
+    try {
+      logger.info({ msg: 'Main Loop started', startTime: new Date(Date.now()) });
+      await run(logger);
+    } catch (err) {
+      const error = err as Error;
+      logger.error({ msg: `Main Loop error: ${error.message}`, err });
+    }
+  }
 
   void tracing.stop();
 }
 
-main();
+void main();
